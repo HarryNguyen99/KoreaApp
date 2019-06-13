6d829984c9d15b97e45fe2c2561b2c04275ffb57
Harry Nguyen <phuquyit@gmail.com>
2019-06-13 11:19:41 +0700

    app korea v2

---
+++ app/src/main/java/com/example/baihoc1/ontap1appkorea/Place.java
@@ -0,0 +1,13 @@
+package com.example.baihoc1.ontap1appkorea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Place extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.place_layout);
+    }
+}
